0beedb4b580c2082ca08e2ea8387b0d9d9d78d73
Patrick Lin <patricksclin@gmail.com>
2017-09-26 18:54:49 +0800

    first commit

---
+++ SearchDemo/ResultItem.swift
@@ -0,0 +1,13 @@
+//
+//  ResultItem.swift
+//  SearchDemo
+//
+//  Created by Patrick Lin on 26/09/2017.
+//  Copyright © 2017 Soocii. All rights reserved.
+//
+
+import UIKit
+
+class ResultItem: NSObject {
+
+}
---
+++ SearchDemo/ResultItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultItemCell.swift
+//  SearchDemo
+//
+//  Created by Patrick Lin on 26/09/2017.
+//  Copyright © 2017 Soocii. All rights reserved.
+//
+
+import UIKit
+
+class ResultItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
